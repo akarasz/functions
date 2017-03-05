f63ef9a9a5b263284df7c500d89dadf0b098f53e
András Kárász <karaszandris@gmail.com>
2017-03-05 10:15:35 +0000

    common aliases for different package managers

--- package-manager.sh
+++ package-manager.sh
@@ -1,14 +1,22 @@
-# yum aliases
-alias yi='sudo yum install'
-alias yr='sudo yum remove'
-alias yu='sudo yum update'
-alias ys='yum search'
-alias yp='yum provides'
+#!/bin/bash
 
-# apt aliases
-alias ai='sudo apt-get install'
-alias ar='sudo apt-get remove'
-alias au='sudo apt-get update && sudo apt-get upgrade'
-alias as='apt-cache search'
-alias yp='apt-file search'
+set_yum_aliases() {
+    alias pkgi='sudo yum install'
+    alias pkgr='sudo yum remove'
+    alias pkgu='sudo yum update'
+    alias pkgs='yum search'
+    alias pkgp='yum provides'
+}
 
+set_apt_aliases() {
+    alias pkgi='sudo apt-get install'
+    alias pkgr='sudo apt-get remove'
+    alias pkgu='sudo apt-get update && sudo apt-get upgrade'
+    alias pkgs='apt-cache search'
+    alias pkgp='apt-file search'
+}
+
+command -v apt-get &>/dev/null && set_apt_aliases && return 0
+command -v yum &>/dev/null && set_yum_aliases && return 0
+
+return 1
