2cc0f8d3c68c0cde385b7fba438b664bec1a2597
András Kárász <karaszandris@gmail.com>
2017-04-01 10:27:04 +0200

    added alias-reload for local aliases

--- utils-aliases.sh
+++ utils-aliases.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
+function alias-reload() {
+    local LOCAL_ALIASES=$HOME/.local/bash_aliases
+
+    . $LOCAL_ALIASES
+}
+
 function alias-add() { local name="$1"
     local command="${@:2}"
 
     local LOCAL_ALIASES=$HOME/.local/bash_aliases
+    mkdir -p $(dirname $LOCAL_ALIASES)
 
     local line="alias $name='$command'"
 
     echo "$line" >> $LOCAL_ALIASES  # persist
-    . $LOCAL_ALIASES
+
+    alias-reload
 }
+
+alias-reload
