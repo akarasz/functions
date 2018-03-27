cbcc680ec2e695071ee87726fe1b6546b7e05164
András Kárász <karaszandris@gmail.com>
2018-03-27 12:15:44 +0200

    rename window while wildfly log tail

--- wildfly.sh
+++ wildfly.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-alias wfl='tail -n 100 -f /opt/wildfly/standalone/log/server.log'
+function wfl() {
+    trap "tmux setw automatic-rename" 0 1 2 5
 
+    tmux rename-window wfl
+    tail -n 100 -f /opt/wildfly/standalone/log/server.log
+    tmux setw automatic-rename
+}
